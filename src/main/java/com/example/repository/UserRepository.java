2cb84f752a21c522574171070e84f166ddf6edac
nosemasashi <ma.nose9413@gmail.com>
2020-03-02 17:39:21 +0900

    UserRepositoryにユーザー情報を登録するinsertメソッドを追加しました。

--- src/main/java/com/example/repository/UserRepository.java
+++ src/main/java/com/example/repository/UserRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
@@ -32,11 +33,23 @@ public class UserRepository {
 		user.setZipcode(rs.getString("zipcode"));
 		user.setAddress(rs.getString("address"));
 		user.setTelephone(rs.getString("telephone"));
-		return  user;
+		return user;
 	};
-	
+
 	@Autowired
 	private NamedParameterJdbcTemplate template;
+
+	/**
+	 * ユーザー情報をDBに登録します.
+	 * 
+	 * @param user ユーザーの情報の入ったオブジェクト
+	 */
+	public void insert(User user) {
+		String sql = "INSERT INTO users (name, email, password, zipcode, address, telephone) VALUES (:name, :email, :password, :zipcode, :address, :telephone)";
+		SqlParameterSource param = new BeanPropertySqlParameterSource(user);
+		template.update(sql, param);
+
+	}
 
 	/**
 	 * メールアドレスからユーザー情報を取得します.
