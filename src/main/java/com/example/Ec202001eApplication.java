48e8ae61c92cc7240ab5f91a39ec3d66de9d819b
KenjiOyamada <oyaken0717@yahoo.co.jp>
2020-03-02 13:51:09 +0900

    【new】first commit

---
+++ src/main/java/com/example/Ec202001eApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ec202001eApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ec202001eApplication.class, args);
+	}
+
+}
