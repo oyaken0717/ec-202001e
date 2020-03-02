9804989783e41e2979b239688ae1be526bbb56d3
nosemasashi <ma.nose9413@gmail.com>
2020-03-02 14:41:16 +0900

    Userドメインを作成しました。

---
+++ src/main/java/com/example/domain/User.java
@@ -0,0 +1,87 @@
+package com.example.domain;
+
+/**
+ * usersテーブルのカラム情報です.
+ * 
+ * @author masashi.nose
+ *
+ */
+public class User {
+	/** ID */
+	private Integer id;
+	/** 名前 */
+	private String name;
+	/** メールアドレス */
+	private String email;
+	/** パスワード */
+	private String password;
+	/** 郵便番号 */
+	private String zipcode;
+	/** 住所 */
+	private String address;
+	/** 電話番号 */
+	private String telephone;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getZipcode() {
+		return zipcode;
+	}
+
+	public void setZipcode(String zipcode) {
+		this.zipcode = zipcode;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getTelephone() {
+		return telephone;
+	}
+
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", email=" + email + ", password=" + password + ", zipcode="
+				+ zipcode + ", address=" + address + ", telephone=" + telephone + "]";
+	}
+
+}
