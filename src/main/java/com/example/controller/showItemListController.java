e10622a2d3a961c96a6df45a1bdb91380f18606f
yusukeAshibe <yusuke.ashibe@rakus-partners.co.jpgit config --listgit config --global user.email yusuke.ashibe@rakus-partners.co.jp>
2020-03-03 11:29:02 +0900

    新規にしshowItemListControllerを作成しました

---
+++ src/main/java/com/example/controller/showItemListController.java
@@ -0,0 +1,35 @@
+package com.example.controller;
+
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.domain.Item;
+import com.example.service.showItemListService;
+
+
+@Controller
+@RequestMapping("/show-itemList")
+public class showItemListController {
+	
+	@Autowired
+	private HttpSession session;
+	
+	@Autowired
+	private showItemListService showitemListServise;
+	
+	@RequestMapping("/")
+	public String showItemList(Model model) {
+		List<Item> itemList = showitemListServise.showItemList();
+		model.addAttribute("itemList",itemList);
+		return "item_list_noodle";
+		
+	}
+	
+
+}
