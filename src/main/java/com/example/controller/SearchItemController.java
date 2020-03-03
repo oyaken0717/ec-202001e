6cf2965cf8a8962cd373a8acd5bcc3314a17002a
yusukeAshibe <yusuke.ashibe@rakus-partners.co.jpgit config --listgit config --global user.email yusuke.ashibe@rakus-partners.co.jp>
2020-03-03 15:41:24 +0900

    SearchItemCOntrollerを名前あいまい検索に対応させました。

--- src/main/java/com/example/controller/SearchItemController.java
+++ src/main/java/com/example/controller/SearchItemController.java
@@ -1,6 +1,5 @@
 package com.example.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.http.HttpSession;
@@ -8,28 +7,38 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.domain.Item;
+import com.example.form.SearchItemForm;
 import com.example.service.SearchItemService;
 
 @Controller
 @RequestMapping("/")
 public class SearchItemController {
 
+	@ModelAttribute
+	public SearchItemForm setUuSearchItemForm() {
+		return new SearchItemForm();
+	}
+
 	@Autowired
 	private HttpSession session;
 
 	@Autowired
-	private SearchItemService showitemListServise;
+	private SearchItemService searchItemService;
 
 	@RequestMapping("/")
-	public String searchItem(Model model) {
-		
-		List<Item> itemList = showitemListServise.showItemList();
-		model.addAttribute("itemList", itemList);
+	public String searchItem(Model model, SearchItemForm form) {
+		if (form.getName() == null) {
+			List<Item> itemList = searchItemService.showItemList();
+			model.addAttribute("itemList", itemList);
+		} else {
+			List<Item> itemList = searchItemService.SearchByLikeName(form.getName());
+			model.addAttribute("itemList", itemList);
+		}
 		return "item_list_noodle";
 
 	}
-
 }
