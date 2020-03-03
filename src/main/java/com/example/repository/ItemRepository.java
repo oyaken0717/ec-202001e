81f25b2079cd8d6f11688295d1ee94d1bda5120a
yusukeAshibe <yusuke.ashibe@rakus-partners.co.jpgit config --listgit config --global user.email yusuke.ashibe@rakus-partners.co.jp>
2020-03-03 11:33:32 +0900

    抜けていた@要素を追加しました

--- src/main/java/com/example/repository/ItemRepository.java
+++ src/main/java/com/example/repository/ItemRepository.java
@@ -5,9 +5,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
 
 import com.example.domain.Item;
-
+@Repository
 public class ItemRepository {
 	
 	 @Autowired
