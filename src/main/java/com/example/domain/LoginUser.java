fcf2221064fa6a4c869b9b08d0c517f9fce701f3
KenjiOyamada <oyaken0717@yahoo.co.jp>
2020-03-03 18:24:32 +0900

    【new】LoginUserクラスの作成

---
+++ src/main/java/com/example/domain/LoginUser.java
@@ -0,0 +1,32 @@
+package com.example.domain;
+
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+
+/**
+ * Userのログイン情報を格納するエンティティ.
+ * 
+ * @author oyamadakenji
+ *
+ */
+public class LoginUser  extends org.springframework.security.core.userdetails.User{
+	private static final long serialVersionUID = 1L;
+	/** User情報 */
+	private final User user;
+	
+	/**
+	 * 通常のUser情報に加え、認可用ロールを設定する。
+	 * 
+	 * @param User　User情報
+	 * @param authorityList 権限情報が入ったリスト
+	 */
+	public LoginUser(User user, Collection<GrantedAuthority> authorityList) {
+		super(user.getEmail(), user.getPassword(), authorityList);
+		this.user = user;
+	}
+
+	public User getAdministrator() {
+		return user;
+	}	
+}
