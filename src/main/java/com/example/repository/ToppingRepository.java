d8efb2d6777922946b5efc65c4c0f0adb64a78e4
nosemasashi <ma.nose9413@gmail.com>
2020-03-04 13:37:38 +0900

    item_detail.html作成、itemリポジトリにloadメソッドを追加しました。

---
+++ src/main/java/com/example/repository/ToppingRepository.java
@@ -0,0 +1,18 @@
+package com.example.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+/**
+ * toppingsテーブルを操作するリポジトリ.
+ * 
+ * @author masashi.nose
+ *
+ */
+@Repository
+public class ToppingRepository {
+	
+	
+	
+
+}
