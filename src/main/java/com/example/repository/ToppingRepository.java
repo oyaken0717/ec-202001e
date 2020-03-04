e0bfa2a40fbaa4b64972d6be4c6e6e8c6bbc196d
nosemasashi <ma.nose9413@gmail.com>
2020-03-04 14:04:46 +0900

    ToppingRepositoryを作成しました。

--- src/main/java/com/example/repository/ToppingRepository.java
+++ src/main/java/com/example/repository/ToppingRepository.java
@@ -1,7 +1,13 @@
 package com.example.repository;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
+
+import com.example.domain.Topping;
 
 /**
  * toppingsテーブルを操作するリポジトリ.
@@ -11,8 +17,31 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public class ToppingRepository {
-	
-	
-	
+
+	@Autowired
+	private NamedParameterJdbcTemplate template;
+
+	/**
+	 * toppingsテーブルの１行分のデータを取り出すローマッパー.
+	 */
+	private static final RowMapper<Topping> TOPPING_ROW_MAPPER = (rs, i) -> {
+		Topping topping = new Topping();
+		topping.setId(rs.getInt("id"));
+		topping.setName(rs.getString("name"));
+		topping.setPriceM(rs.getInt("price_m"));
+		topping.setPriceL(rs.getInt("price_l"));
+		return topping;
+	};
+
+	/**
+	 * toppingsテーブルから全件検索します.
+	 * 
+	 * @return Topping情報の詰まったオブジェクト.
+	 */
+	public List<Topping> findAll() {
+		String sql = "SELECT id, name, price_m, price_l from toppings ORDER BY id";
+		List<Topping> toppingList = template.query(sql, TOPPING_ROW_MAPPER);
+		return toppingList;
+	}
 
 }
