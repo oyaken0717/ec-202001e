d4750f4f6637b732207a4573fb5c1c510aedb124
yusukeAshibe <yusuke.ashibe@rakus-partners.co.jpgit config --listgit config --global user.email yusuke.ashibe@rakus-partners.co.jp>
2020-03-05 13:22:39 +0900

    誤って変更してしまったsecuritycibfugの変更を元に戻しました。

--- src/main/java/com/example/SecurityConfig.java
+++ src/main/java/com/example/SecurityConfig.java
@@ -16,7 +16,7 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 /**
  * ログイン認証用設定.
  * 
- * @author kenjioyamada
+ * @author igamasayuki
  *
  */
 @Configuration // 設定用のクラス
@@ -43,26 +43,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	 */
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		
 
 		http.authorizeRequests()
-
 //			.antMatchers("/login-user/to-login","/register-user/toRegister").permitAll() 
-<<<<<<< HEAD
 				.antMatchers("/**").permitAll().antMatchers("/user/**").hasRole("USER") // /user/から始まるパスはUSER権限でログインしている場合のみアクセス可(権限設定時の「ROLE_」を除いた文字列を指定)
-=======
-				.antMatchers("/**").permitAll()
-				.antMatchers("/user/**")
-				.hasRole("USER") // /user/から始まるパスはUSER権限でログインしている場合のみアクセス可(権限設定時の「ROLE_」を除いた文字列を指定)
->>>>>>> origin/develop
 				.anyRequest().authenticated(); // それ以外のパスは認証が必要
 
 		http.formLogin() // ログインに関する設定
 				.loginPage("/login-user/to-login") // ログイン画面に遷移させるパス(ログイン認証が必要なパスを指定してかつログインされていないとこのパスに遷移される)
 				.loginProcessingUrl("/login-user/login") // ログインボタンを押した際に遷移させるパス(ここに遷移させれば自動的にログインが行われる)
-				.failureUrl("/login-user/to-login") // ログイン失敗に遷移させるパス
+				.failureUrl("/error") // ログイン失敗に遷移させるパス
 //			.defaultSuccessUrl("/login-user/login", false) // 第1引数:デフォルトでログイン成功時に遷移させるパス
-			.defaultSuccessUrl("/searchItem/", false) // 第1引数:デフォルトでログイン成功時に遷移させるパス
+//			.defaultSuccessUrl("/item_list_noodle", false) // 第1引数:デフォルトでログイン成功時に遷移させるパス
 				// 第2引数: true :認証後常に第1引数のパスに遷移
 				// false:認証されてなくて一度ログイン画面に飛ばされてもログインしたら指定したURLに遷移
 				.usernameParameter("email") // 認証時に使用するユーザ名のリクエストパラメータ名(今回はメールアドレスを使用)
@@ -70,7 +62,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 		http.logout() // ログアウトに関する設定
 				.logoutRequestMatcher(new AntPathRequestMatcher("/login-user/logout")) // ログアウトさせる際に遷移させるパス
-				.logoutSuccessUrl("/searchItem/") // ログアウト後に遷移させるパス(ここではログイン画面を設定)
+				.logoutSuccessUrl("/login-user/to-login") // ログアウト後に遷移させるパス(ここではログイン画面を設定)
 				.deleteCookies("JSESSIONID") // ログアウト後、Cookieに保存されているセッションIDを削除
 				.invalidateHttpSession(true); // true:ログアウト後、セッションを無効にする false:セッションを無効にしない
 
