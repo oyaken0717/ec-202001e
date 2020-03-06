80b86abd829e4a218b08620f6f05b84617521885
nosemasashi <ma.nose9413@gmail.com>
2020-03-06 15:25:17 +0900

    商品詳細ページのカンマ表示を修正しました。

--- src/main/resources/static/js/item_detail.js
+++ src/main/resources/static/js/item_detail.js
@@ -23,14 +23,16 @@ $(function() {
 		var topping_count = $("#topping input:checkbox:checked").length;
 		var ramen_num = $("#ramennum option:selected").text();
 		if (size == "M") {
-			var size_price = $("#sizeM").text();
+			var size_priceM_text = $("#sizeM").text();
+			var size_price = parseInt(size_priceM_text.replace(/,/, '') );
 			var topping_price = 200 * topping_count;
 		} else {
-			var size_price = $("#sizeL").text();
+			var size_priceL_text = $("#sizeL").text();
+			var size_price = parseInt(size_priceL_text.replace(/,/, '') );
 			var topping_price = 300 * topping_count;
 		}
 		
-		var price = (parseInt(size_price) + topping_price) * ramen_num;
+		var price = (size_price + topping_price) * ramen_num;
 		$("#totalprice").text(price.toLocaleString());
 	}
 	;
