576ddaa498830e4dc4731ec38f94e8cfed99eaaa
yusukeAshibe <yusuke.ashibe@rakus-partners.co.jpgit config --listgit config --global user.email yusuke.ashibe@rakus-partners.co.jp>
2020-03-09 11:26:56 +0900

    controllerを修正しました

--- src/main/java/com/example/controller/SearchItemController.java
+++ src/main/java/com/example/controller/SearchItemController.java
@@ -21,9 +21,8 @@ import com.example.service.SearchItemService;
  *
  */
 @Controller
-@RequestMapping("/")
-public  class SearchItemController   {
-
+@RequestMapping("")
+public class SearchItemController {
 
 	/**
 	 * searchItemFormをインスタンス化
@@ -34,6 +33,7 @@ public  class SearchItemController   {
 	public SearchItemForm setUuSearchItemForm() {
 		return new SearchItemForm();
 	}
+
 	@ModelAttribute
 	public SortItemForm setUpSortItemForm() {
 		return new SortItemForm();
@@ -45,26 +45,29 @@ public  class SearchItemController   {
 	/**
 	 * 商品一覧への遷移（あいまい検索された場合はその商品のみ表示）.
 	 * 
-	 * @param model モデル
+	 * @param model          モデル
+	 * @param searchItemForm
 	 * @param form
 	 * @return 商品一覧表示画面
 	 */
-	@RequestMapping("/")
-	public String showItemList(Model model) {
-
+	@RequestMapping("")
+	public String showItemList(Model model, SearchItemForm searchItemForm) {
 		List<Item> itemList = searchItemService.showItemList();
 		List<Item> threeList = new ArrayList<>();
 		List<List<Item>> bigItemList = new ArrayList<>();
+
 		for (int i = 0; i < itemList.size(); i++) {
 			threeList.add(itemList.get(i));
 			if (threeList.size() == 3) {
 				bigItemList.add(threeList);
 				threeList = new ArrayList<>();
 			}
-		}
-		model.addAttribute("bigItemList", bigItemList);
 
+		}
+
+		model.addAttribute("bigItemList", bigItemList);
 		return "item_list_noodle";
+
 	}
 
 	/**
@@ -75,14 +78,19 @@ public  class SearchItemController   {
 	 * @return 商品一覧表示画面
 	 */
 	@RequestMapping("/searchItem")
-	public String searchItemList(Model model, SearchItemForm searchItemform,SortItemForm sortItemForm) {
-		List<Item> itemList = searchItemService.SearchByLikeName(searchItemform.getName());
+	public String searchItemList(Model model, SearchItemForm searchItemForm) {
+		List<List<Item>> bigItemList = showItemList(searchItemForm, model);
+		model.addAttribute("bigItemList", bigItemList);
+		return "item_list_noodle";
+
+	}
+
+	public List<List<Item>> showItemList(SearchItemForm searchItemForm, Model model) {
+		List<Item> itemList = searchItemService.SearchByLikeName(searchItemForm.getName());
 		List<Item> threeList = new ArrayList<>();
 		List<List<Item>> bigItemList = new ArrayList<>();
-
 		if (itemList.size() == 0) {
 			model.addAttribute("message", "該当する商品がありません");
-			itemList = searchItemService.showItemList();
 			for (int i = 0; i < itemList.size(); i++) {
 				threeList.add(itemList.get(i));
 				if (threeList.size() == 3) {
@@ -90,35 +98,26 @@ public  class SearchItemController   {
 					threeList = new ArrayList<>();
 				}
 			}
-			model.addAttribute("bigItemList", bigItemList);
-
-		} else {
-			if (itemList.size() < 3) {
-				for (int i = 0; i < itemList.size(); i++) {
-					threeList.add(itemList.get(i));
-				} 
-				bigItemList.add(threeList);
-			} else {
-				for (int i = 0; i < itemList.size(); i++) {
-					threeList.add(itemList.get(i));
-					if (threeList.size() == 3){
-						bigItemList.add(threeList);
-						threeList = new ArrayList<>();
-					}
-					
-				}
-				if(threeList.size()<3) {
-					bigItemList.add(threeList);
-				}
-
+		} else if (itemList.size() > 1 && itemList.size() < 3) {
+			for (int i = 0; i < itemList.size(); i++) {
+				threeList.add(itemList.get(i));
 			}
-			model.addAttribute("bigItemList", bigItemList);
-
+			bigItemList.add(threeList);
+		} else {
+			for (int i = 0; i < itemList.size(); i++) {
+				threeList.add(itemList.get(i));
+				if (threeList.size() == 3) {
+					bigItemList.add(threeList);
+					threeList = new ArrayList<>();
+				}
+			}
+			if (threeList.size() < 3) {
+				bigItemList.add(threeList);
+			}
 		}
-		return "item_list_noodle";
+		model.addAttribute("bigItemList", bigItemList);
+		return bigItemList;
 	}
-	
-	
-}
 
+}
 
